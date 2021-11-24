8df298c7da7bd6f73d54a2d8cfa531814b2c6be7
pringle97 <malia.pringle@gmail.com>
2021-11-24 00:09:06 -0800

    outline

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=43d49819fe899466b30d41716359a5fd`)
+
+//search button function showing weather conditions of searched city
+    //display city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+//UV index changes colors based on severity of weather
+document.getElementById('search').addEventListener('click', event => {
+  event.preventDefault()
+  const cityName = document.getElementById('searchCity').value
