5c17a4e5bdb5cbce01ecd4aada1b40ef44083e7b
pringle97 <malia.pringle@gmail.com>
2021-12-11 21:43:09 -0800

    restarting

--- script.js
+++ script.js
@@ -1,57 +1,33 @@
+// const api_key = 'f3f2b60e59009428800ffe45c8e776e4'
 //search button function showing weather conditions of searched city
 //display city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 //UV index changes colors based on severity of weather
 
-let weather = {
-  apiKey: "f3f2b60e59009428800ffe45c8e776e4",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&units=metric&appid=" +
-      this.apiKey
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-  },
-  search: function () { 
-    this.fetchWeather(document.querySelector(".search-bar").value)
-  }
-};
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
-
-document.querySelector(".search-bar").addEventListener
-  if (event.key == "Enter") {
-    weather.search
-  }
 //creating search function to search weather by city name
-// document.getElementById('search').addEventListener('click', event => {
-//   event.preventDefault()
-//   const cityName = document.getElementById('name')
-//   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=f3f2b60e59009428800ffe45c8e776e4`)
-//     .then(res => {
-//       const weather = res.data
-//       console.log(res.data)
-//     })
-// })
+
+document.getElementById('search').addEventListener('click', event => {
+  event.preventDefault()
+  const cityName = document.getElementById('searchCity').value
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=f3f2b60e59009428800ffe45c8e776e4`)
+    .then(res => {
+      const weather = res.data
+      document.getElementById('city').innerHTML = `
+        <h1>${weather.name}</h1>
+      `
+      document.getElementById('temp').innerHTML = `
+        <h2>${weather.main.temp}°F</h2>
+      `
+      document.getElementById('humidity').innerHTML = `
+        <div>${weather.}<div>
+      `
+      // document.getElementById('wind').innerHTML = `
+
+      // `
+      // document.getElementById('').innerHTML = `
+
+      // `
+      console.log(res.data)
+    })
+})
+
